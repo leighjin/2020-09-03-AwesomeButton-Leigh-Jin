a04eced5bb1c8ae2528fe101af985a3805be5718
Leigh Jin <jinlei@sfsu.edu>
2020-09-03 12:48:43 -0700

    added label and button, created IBOutlet and IBAction

--- 2020-09-03-AwesomeButton-Leigh-Jin/ViewController.swift
+++ 2020-09-03-AwesomeButton-Leigh-Jin/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //add IBOutlet
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You are Awesome !"
+    }
+    
+    
+    
 }
 
